f6398ded611bfb16cb4500b39607736f086b31d6
marcosiraola <iraolamarcos@gmail.com>
2022-07-14 02:33:27 -0300

    graph, imgs, and translations

--- src/components/projectsCard/ProjectCard.view.js
+++ src/components/projectsCard/ProjectCard.view.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './projectCard.module.css';
 import { useTranslation } from 'react-i18next';
+import Javascript from '../../assets/javascript.png';
+import Vue from '../../assets/vue.png';
+import Java from '../../assets/java.png';
+import CSS from '../../assets/css.png';
+import html from '../../assets/html.png';
+import PHP from '../../assets/php.png';
 
 const ProjectCard = ( props ) => {
 
     const { t } = useTranslation();
+
+    const dynamicImages = (param) => {
+        switch(param) {
+            case 'JavaScript':
+                return Javascript;
+            case 'Vue':
+                return Vue;
+            case 'Java':
+                return Java;
+            case 'CSS':
+                return CSS;
+            case 'PHP':
+                return PHP;
+            default:
+                return null;
+        }
+    }
 
     return (
         <div className={styles.container}>
@@ -13,6 +36,14 @@ const ProjectCard = ( props ) => {
             <p>{ t('repos_card.language') }: { props.language }</p>
             <p>{ t('repos_card.update') }: { props.update }</p>
             <a className={ styles.url } href={props.url}>{ t('repos_card.goto') }</a>
+            
+            {
+                props.language ?
+                <img className={styles.logo} src={dynamicImages(props.language)} alt='Lang logo' />
+                :
+                <></>
+            }
+
         </div>
     )
 }
--- src/pages/home/home.view.js
+++ src/pages/home/home.view.js
@@ -5,7 +5,7 @@ import { API_URL } from '../../routes/routes';
 import { Skeleton } from 'antd';
 import 'antd/dist/antd.css';
 import { useTranslation } from 'react-i18next';
-
+import Chart from 'react-apexcharts';
 
 const Home = () => {
 
@@ -14,8 +14,22 @@ const Home = () => {
     const [user, setUser] = useState(null);
     const [repos, setRepos] = useState(null);
 
-    // const [githubUser, setGithubUser] = useState(API_URL);
-    // console.log(githubUser);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [jsRepos, setJsRepos] = useState(0);
+    const [vueRepos, setVueRepos] = useState(0);
+    const [phpRepos, setPhpRepos] = useState(0);
+    const [cssRepos, setCssRepos] = useState(0);
+    const [javaRepos, setJavaRepos] = useState(0);
+    const [otherRepos, setOtherRepos] = useState(0);
+
+    const [langs, setLangs] = useState([]);
+
+    const state = {
+        options: {},
+        series: [jsRepos, vueRepos, phpRepos, cssRepos, javaRepos, otherRepos],
+        labels: langs
+    }
 
     useEffect(() => {
         const url = API_URL;
@@ -32,7 +46,8 @@ const Home = () => {
             return Promise.reject(response.status)
         })
         .then(payload => {
-            setUser(payload)
+            setUser(payload);
+            setIsLoading(false);
         })
         .catch(error => console.log(error))
 
@@ -53,18 +68,66 @@ const Home = () => {
             return Promise.reject(response.status)
         })
         .then(payload => {
-            setRepos(payload)
+            setRepos(payload);
+            sumProjects(payload);
         })
         .catch(error => console.log(error))
 
     }, []);
+
+    const sumProjects = (props) => {
+
+        let js = 0;
+        let vue = 0;
+        let php = 0;
+        let css = 0;
+        let java = 0;
+        let other = 0;
+
+        props.forEach(repo => {
+            
+            if(!langs.includes(props.language)){
+                langs.push(props.language);
+            }
+
+            if(repo.language == "JavaScript") {
+                js = js + 1;
+                setJsRepos(js);
+            }
+
+            if(repo.language == "Vue") {
+                vue = vue + 1;
+                setVueRepos(vue);
+            }
+
+            if(repo.language == "PHP") {
+                php = php + 1;
+                setPhpRepos(php);
+            }
+
+            if(repo.language == "CSS") {
+                css = css + 1;
+                setCssRepos(css);
+            }
+
+            if(repo.language == "Java") {
+                java = java + 1;
+                setJavaRepos(java);
+            }
+
+            if(repo.language == null) {
+                other = other + 1;
+                setOtherRepos(other);
+            }
+        });
+    }
     
     return (
         <div className={styles.home}>
             <p className={styles.title}>{ t('home.title') }</p>
 
             <div className={styles.user_container}>
-                {user === null ?
+                {isLoading ?
                     <Skeleton className={styles.skeleton} active />
                     :
                     <>
@@ -72,9 +135,12 @@ const Home = () => {
                         <p>{ t('home.company') }: { user?.company }</p>
                         <p>{ t('home.location') }: { user?.location }</p>
                         <p>{ t('home.public_repos') }: { user?.public_repos }</p>
+                        <p>{ t('home.bio') }: { user?.bio }</p>
                     </>
                 }
             </div>
+
+            <Chart options={state.options} series={state.series} labels={state.labels} type="donut" width="380" />
 
             <div className={styles.repos_container}>
                 <ListProjectsCard repos={ repos }/>
